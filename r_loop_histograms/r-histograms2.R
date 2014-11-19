12220a51669f2c1e1268cc5b665016d89b8bf2da
nygeog <daniel.martin.sheehan@gmail.com>
2014-11-18 23:30:22 -0500

    update

--- r_loop_histograms/r-histograms2.R
+++ r_loop_histograms/r-histograms2.R
@@ -2,11 +2,26 @@ library(ggplot2)
 
 df <- read.csv('pollen_sites.csv')
 
-for (name in colnames(df)){
-  x = paste(c('histograms/',name,'.png'), collapse='')
-  print(x)
-  qplot(name, data=df, geom="histogram")
-  dev.copy(png,filename=x);
-  #graphics.off();
-  dev.off();
-}
+# for (name in colnames(df)){
+#   x = paste(c('histograms/',name,'.png'), collapse='')
+#   print(x)
+#   png(filename=x)
+#   qplot(name, data=df, geom="histogram")
+#   dev.copy(png) #;
+#   #graphics.off() #;
+#   #dev.off() #;
+#   dev.off() #;
+# }
+
+
+png(filename="histograms/plot50.png")
+qplot(pr1000mbldgbulkland, data=df, geom="histogram")
+dev.copy(png)
+graphics.off()
+#dev.off()
+
+png(filename="histograms/plot51.png")
+qplot(pr0500mbldgbulkland, data=df, geom="histogram")
+dev.copy(png)
+dev.off()
+dev.off()
