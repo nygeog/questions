949ad4bdf5ede8144dd32e9209c8fd37a6eb5a17
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2014-12-04 17:39:06 -0500

    update

--- r_loop_histograms/r-histograms2.R
+++ r_loop_histograms/r-histograms2.R
@@ -1,27 +1,14 @@
 library(ggplot2)
 
+setwd("/Users/danielmsheehan/GitHub/questions/r_loop_histograms/histograms/")
 df <- read.csv('pollen_sites.csv')
+head(df)[1:5]
+for (ii in 2:ncol(df)){
+  png(filename=paste("plot",colnames(df)[ii], sep="_" ))
+  #p<-qplot(name, data=df, geom="histogram")
+  p<-ggplot(data=df, aes(x=df[, 1], y=df[,ii]))+geom_point()+
+    xlab(paste(colnames(df)[1]))+ylab(paste(colnames(df)[ii]))
+  print(p)
 
-# for (name in colnames(df)){
-#   x = paste(c('histograms/',name,'.png'), collapse='')
-#   print(x)
-#   png(filename=x)
-#   qplot(name, data=df, geom="histogram")
-#   dev.copy(png) #;
-#   #graphics.off() #;
-#   #dev.off() #;
-#   dev.off() #;
-# }
-
-
-png(filename="histograms/plot50.png")
-qplot(pr1000mbldgbulkland, data=df, geom="histogram")
-dev.copy(png)
-graphics.off()
-#dev.off()
-
-png(filename="histograms/plot51.png")
-qplot(pr0500mbldgbulkland, data=df, geom="histogram")
-dev.copy(png)
-dev.off()
-dev.off()
+  dev.off() 
+}
